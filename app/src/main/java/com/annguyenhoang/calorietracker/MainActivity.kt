598ff1a5850f7d45b39960c42570edde35fcc6e4
an.nguyenh <an.nguyenh@s3corp.com.vn>
2024-04-25 18:00:06 +0700

    Add gender screen

--- app/src/main/java/com/annguyenhoang/calorietracker/MainActivity.kt
+++ app/src/main/java/com/annguyenhoang/calorietracker/MainActivity.kt
@@ -3,15 +3,22 @@ package com.annguyenhoang.calorietracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.annguyenhoang.calorietracker.navigation.navigate
 import com.annguyenhoang.calorietracker.ui.theme.CalorieTrackerTheme
 import com.annguyenhoang.core.navigation.Route
+import com.annguyenhoang.onboarding_presentation.gender.GenderScreen
 import com.annguyenhoang.onboarding_presentation.welcome.WelcomeScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -25,31 +32,31 @@ class MainActivity : ComponentActivity() {
                         WelcomeScreen(onNavigate = navController::navigate)
                     }
                     composable(Route.AGE) {
-
+                        Box(modifier = Modifier.fillMaxSize())
                     }
                     composable(Route.GENDER) {
-
+                        GenderScreen(onNavigate = navController::navigate)
                     }
                     composable(Route.HEIGHT) {
-
+                        Box(modifier = Modifier.fillMaxSize())
                     }
                     composable(Route.WEIGHT) {
-
+                        Box(modifier = Modifier.fillMaxSize())
                     }
                     composable(Route.NUTRIENT_GOAL) {
-
+                        Box(modifier = Modifier.fillMaxSize())
                     }
                     composable(Route.ACTIVITY) {
-
+                        Box(modifier = Modifier.fillMaxSize())
                     }
                     composable(Route.GOAL) {
-
+                        Box(modifier = Modifier.fillMaxSize())
                     }
                     composable(Route.TRACKER_OVERVIEW) {
-
+                        Box(modifier = Modifier.fillMaxSize())
                     }
                     composable(Route.SEARCH) {
-
+                        Box(modifier = Modifier.fillMaxSize())
                     }
                 }
             }
